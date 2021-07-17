39ded464351e6927e3a5d041afde4820d1745b63
Lucas Nadalete <lucas.nadalete@focusnetworks.com.br>
2021-07-17 00:42:06 -0300

    Initial version of iacit_sample generated by generator-jhipster@7.1.0

---
+++ src/main/java/br/com/iacit/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package br.com.iacit.repository;
+
+import br.com.iacit.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/br/com/iacit/IntegrationTest.java
@@ -0,0 +1,17 @@
+package br.com.iacit;
+
+import br.com.iacit.IacitSampleApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = IacitSampleApp.class)
+public @interface IntegrationTest {
+}
